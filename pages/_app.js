9845db930c971759054747d728085b9b4cffe1a8
Luis Roldan <roldanluis022@gmail.com>
2026-03-25 21:25:04 -0700

    Add deploy workflow

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/Home.module.css";
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+
+export default function App({ Component: _Component, pageProps }) {
+	return <_Component {...pageProps} />;
 }
